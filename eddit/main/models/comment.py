4b08fedf9a4a29f4bd007887a7d582708f18a786
Ryan Batchelder <rb688@nau.edu>
2015-03-02 16:10:02 -0700

    Added comment and post models

---
+++ eddit/main/models/comment.py
@@ -0,0 +1,10 @@
+from django.db import models
+from main.models import user
+from main.models.post import Post
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(user)
+    parent_post = models.ForeignKey(Post)
+    comment_date = models.DateTimeField(auto_now=True)
+    comment_content = models.TextField()
