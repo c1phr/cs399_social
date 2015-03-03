b4ba49b6d15d7da7c6338f7aa2cfbd7dce88ddb4
kyleamcginn88 <kyle.a.mcginn@gmail.com>
2015-03-03 16:07:16 -0700

    Added post form and comment forms

--- eddit/main/forms.py
+++ eddit/main/forms.py
@@ -4,11 +4,20 @@ from django.http import HttpResponseRedirect
 from django.db import models
 from main.models import UserProfile
    
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
+        exclude = []
 
     phone_number = forms.CharField(max_length=15, required=True)
     first_name = forms.CharField(max_length=128, required=True)
     last_name = forms.CharField(max_length=128, required=True)
     user_name = forms.CharField(max_length=128, required=True)
+    
+
+class PostForm(forms.ModelForm):
+    post_data = forms.CharField(max_length=256, required=True)
+    
+class CommentForm(forms.ModelForm):
+    comment_data = forms.CharField(max_length=256, required=True)
