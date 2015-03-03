c4ee8f8feb0d3236395317ac0e939f97cb3228a8
kyleamcginn88 <kyle.a.mcginn@gmail.com>
2015-03-03 16:11:31 -0700

    Added imports for models in forms.py

--- eddit/main/forms.py
+++ eddit/main/forms.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django import forms
 from django.http import HttpResponseRedirect
 from django.db import models
-from main.models import UserProfile
+from main.models import UserProfile, Post, Comment
    
 
 class UserProfileForm(forms.ModelForm):
@@ -10,12 +10,13 @@ class UserProfileForm(forms.ModelForm):
         model = UserProfile
         exclude = []
 
+    email = forms.EmailField(label="Your email", required=True)
     phone_number = forms.CharField(max_length=15, required=True)
     first_name = forms.CharField(max_length=128, required=True)
     last_name = forms.CharField(max_length=128, required=True)
     user_name = forms.CharField(max_length=128, required=True)
     
-
+    
 class PostForm(forms.ModelForm):
     post_data = forms.CharField(max_length=256, required=True)
     
