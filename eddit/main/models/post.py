bbe1af0bb5d171ab959fa9fa7f154eb83100660f
Ryan Batchelder <rb688@nau.edu>
2015-04-02 21:23:04 -0700

    Added the ability to make new posts with live reload

--- eddit/main/models/post.py
+++ eddit/main/models/post.py
@@ -6,5 +6,5 @@ class Post(models.Model):
     id = models.IntegerField(max_length=10, primary_key=True, editable=False)
     author = models.ForeignKey(User)
     post_title = models.TextField(max_length=50)
-    post_date = models.DateTimeField(auto_now=True)
+    post_date = models.DateTimeField(auto_now=True, editable=False)
     post_link = models.TextField()
