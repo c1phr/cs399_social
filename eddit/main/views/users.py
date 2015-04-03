d7a63abc1e940d47c68fc2984248e0de000a69bb
Ryan Batchelder <rb688@nau.edu>
2015-04-03 07:50:10 -0700

    Added dynamic user page functionality

--- eddit/main/views/users.py
+++ eddit/main/views/users.py
@@ -3,25 +3,16 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from main.models.post import Post
+
 
 @login_required
 def users(request):
 	user_name = request.GET.get('username', None)
-	return render(request, "main/user.html", {'all_users': User.objects.all(), 'user' : User.objects.filter(username=user_name)})
+	return render(request, "main/user.html", {'all_users': User.objects.all(), 'current_user' : User.objects.filter(username=user_name)})
 
 @login_required
 def profile(request):
-	user_name = request.GET.get(pk=int(id))
-	return render(request, "main/profile.html", { 'user' : User.objects.get(pk=int(id))})
-
-@login_required
-def ryan(request):
-	return render(request, "main/dooley.html")
-
-@login_required
-def keevan(request):
-	return render(request, "main/keevan.html")
-
-@login_required
-def justin(request):
-	return render(request, "main/justin.html")
+	userid = request.GET.get("id", "")
+	user = User.objects.filter(id = userid).first()
+	return render(request, "main/profile.html", { 'user' : user, 'posts': Post.objects.filter(author=user)})
