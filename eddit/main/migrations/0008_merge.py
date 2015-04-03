448eb16cf609a111337876a75c4e98a25db18278
Brandon P <brandonp@nau.edu>
2015-04-02 22:47:04 -0700

    Added upvotes to database for posts, no functionality yet.

---
+++ eddit/main/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0007_remove_post_upvotes'),
+        ('main', '0006_delete_userprofile'),
+    ]
+
+    operations = [
+    ]
